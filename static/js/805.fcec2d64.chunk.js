6096bfb194d96747e332af8a6a0d8466c374e4e3
chemvic <112618402+chemvic@users.noreply.github.com>
2023-05-29 10:51:29 +0000

    Deploying to gh-pages from @ chemvic/goit-react-hw-08-phonebook@e39250d9792ef5665c034ca907313d80fb60c556 🚀

---
+++ static/js/805.fcec2d64.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r="Home_container__3pR7L",a=t(1123),c=t(184),o=function(){return(0,c.jsx)("div",{className:r,children:(0,c.jsx)(a.Z,{variant:"h4",children:"Phone book welcome page"})})}}}]);
+//# sourceMappingURL=805.fcec2d64.chunk.js.map
